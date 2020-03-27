d71981ee9775ef0f136d5f9325c2a2171a26f670
Peter Hanjun <peter_hanjun@hotmail.com>
2020-03-27 17:29:38 +0800

    Initial.

--- src/main/java/com/smart/Car.java
+++ src/main/java/com/smart/Car.java
@@ -1,9 +1,53 @@
 package com.smart;
 
-public class Car {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+
+/**
+ * Bean Life Cycle Interface
+ * BeanFactoryAware    -      setBeanFactory()
+ * BeanNameAware       -      setBeanName()
+ * InitializingBean    -      afterPropertiesSet()
+ * DisposableBean      -      destroy()
+ */
+
+public class Car implements BeanFactoryAware, BeanNameAware, InitializingBean, DisposableBean {
     private String brand;
     private String color;
     private int maxSpeed;
+
+    private BeanFactory beanFactory;
+    private String beanName;
+
+    //BeanFactoryAware
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryAware - setBeanFactory:" + beanFactory);
+        this.beanFactory = beanFactory;
+    }
+    //BeanNameAware
+    public void setBeanName(String beanName) {
+        System.out.println("BeanNameAware - setBeanName:"+ beanName);
+        this.beanName = beanName;
+    }
+    //DisposableBean
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean - destroy");
+    }
+    //InitializingBean
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean - afterPropertiesSet");
+    }
+
+    //bean property init-method define
+    public void myInit(){
+        System.out.println("call <init-method> defined myInit() - max speed change to 240");
+        setMaxSpeed(240);
+    }
+
+    //bean property destroy-method define
+    public void myDestroy(){
+        System.out.println("call <destroy-method defined myDestroy()");
+    }
 
     public void introduce(){
         System.out.println("Car Brand:" + brand + " ,color:"+color+" ,max speed:"+maxSpeed);
@@ -14,6 +58,7 @@ public class Car {
     }
 
     public void setBrand(String brand) {
+        System.out.println("setBrand");
         this.brand = brand;
     }
 
@@ -22,6 +67,7 @@ public class Car {
     }
 
     public void setColor(String color) {
+        System.out.println("setColor");
         this.color = color;
     }
 
@@ -30,6 +76,17 @@ public class Car {
     }
 
     public void setMaxSpeed(int maxSpeed) {
+        System.out.println("setSpeed");
         this.maxSpeed = maxSpeed;
     }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+
 }
